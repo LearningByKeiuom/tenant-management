35f9299a56c78d208fe3619e8fb15867b623433c
Farhankaioum <keiuom.swe@gmail.com>
2025-10-23 23:23:34 +1100

    SchoolService interface added

---
+++ Infrastructure/Services/Schools/ISchoolService.cs
@@ -0,0 +1,11 @@
+namespace Infrastructure.Services.Schools;
+
+public interface ISchoolService
+{
+    Task<IResponseWrapper<List<SchoolResponse>>> GetAllAsync();
+    Task<IResponseWrapper<int>> CreateAsync(CreateSchoolRequest request);
+    Task<IResponseWrapper<int>> UpdateAsync(UpdateSchoolRequest request);
+    Task<IResponseWrapper<int>> DeleteAsync(string schoolId); 
+    Task<IResponseWrapper<SchoolResponse>> GetByIdAsync(string schoolId);
+    Task<IResponseWrapper<SchoolResponse>> GetByNameAsync(string schoolName);
+}
