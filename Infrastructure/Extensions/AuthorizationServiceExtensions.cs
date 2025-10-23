f945cdbf6c04e589f080d22682571d0c05cecd79
Farhankaioum <keiuom.swe@gmail.com>
2025-10-23 23:56:29 +1100

    AuthorizationService extension added

---
+++ Infrastructure/Extensions/AuthorizationServiceExtensions.cs
@@ -0,0 +1,13 @@
+namespace Infrastructure.Extensions;
+
+public static class AuthorizationServiceExtensions
+{
+    public static async Task<bool> HasPermissionAsync(
+        this IAuthorizationService service, 
+        ClaimsPrincipal user, 
+        string feature, 
+        string action)
+    {
+        return (await service.AuthorizeAsync(user, null, SchoolPermission.NameFor(action, feature))).Succeeded;
+    }
+}
