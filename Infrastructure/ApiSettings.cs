c8e72540f527ce73e94c67b3c3befdeaefc374ac
Farhankaioum <keiuom.swe@gmail.com>
2025-10-23 23:28:38 +1100

    UserEndpoints added

--- Infrastructure/ApiSettings.cs
+++ Infrastructure/ApiSettings.cs
@@ -15,3 +15,19 @@ public class TokenEndpoints
     public string Login { get; set; }
     public string RefreshToken { get; set; }
 }
+
+public class UserEndpoints
+{
+    public string Update { get; set; }
+    public string ResetPassword { get; set; }
+    public string All { get; set; }
+    public string ById { get; set; }
+    public string Register { get; set; }
+    public string RolesById { get; set; }
+    public string UpdateRoles { get; set; }
+    public string UpdateStatus { get; set; }
+
+    public string GetById(string userId) =>  $"{ById}{userId}";
+    public string GetRolesById(string userId) =>  $"{RolesById}{userId}";
+    public string UpdateRolesById(string userId) =>  $"{UpdateRoles}{userId}";
+}
