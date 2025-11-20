1d076d1349ba6782040161cf6c86d034a263e066
Farhankaioum <keiuom.swe@gmail.com>
2025-11-20 21:09:08 +1100

    NSwag.AspNetCore added

--- WebApi-minimal/obj/Debug/net8.0/WebApi-minimal.AssemblyInfo.cs
+++ WebApi-minimal/obj/Debug/net8.0/WebApi-minimal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi-minimal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e4da3a0fde452fd885223c48042b1b520fb9297")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef23add5634251c8bc00ccbd65ce4fdd384b2ab2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi-minimal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi-minimal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
