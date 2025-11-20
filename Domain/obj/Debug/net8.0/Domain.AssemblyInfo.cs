bb4a10609a296282227e6454f261227d9b0aa02b
Farhankaioum <keiuom.swe@gmail.com>
2025-11-20 21:04:28 +1100

    Finbuckle.MultiTenant added

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30f0a1f3c5110394dc0271abca312b73feca4607")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf5347b38c016799347c2fdb864a55e43ebf3b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
