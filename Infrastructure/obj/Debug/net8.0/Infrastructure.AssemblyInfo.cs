575a6bae92e44ae00e1b4dbd22f2d071a4ce1e55
Farhankaioum <keiuom.swe@gmail.com>
2025-11-21 17:56:33 +1100

    ApplicationUser Entity model added

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+998f1b397af8bd474e64b40af6fa7283a36d34d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90cbc48804cf27cca17eb33977d3a8dd9178f232")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
