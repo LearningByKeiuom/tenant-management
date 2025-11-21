11372bca8a8f0a268696f5f5cc70a7af12537d66
Farhankaioum <keiuom.swe@gmail.com>
2025-11-21 16:12:16 +1100

    ClaimConstant added

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d076d1349ba6782040161cf6c86d034a263e066")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c66f9656bbf55c3802ba4083f475991d5cfb6b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
