9a1701c5c20af0a06ae8719abf17d67a2a31f024
Farhankaioum <keiuom.swe@gmail.com>
2025-11-21 16:15:44 +1100

    SchoolFeature added

--- Infrastructure/Constants/PermissionConstants.cs
+++ Infrastructure/Constants/PermissionConstants.cs
@@ -9,3 +9,14 @@ public static class SchoolAction
     public const string RefreshToken = nameof(RefreshToken);
     public const string UpgradeSubscription = nameof(UpgradeSubscription);
 }
+
+public static class SchoolFeature
+{
+    public const string Tenants = nameof(Tenants);
+    public const string Users = nameof(Users);
+    public const string Roles = nameof(Roles);
+    public const string UserRoles = nameof(UserRoles);
+    public const string RoleClaims = nameof(RoleClaims);
+    public const string Schools = nameof(Schools);
+    public const string Tokens = nameof(Tokens);
+}
