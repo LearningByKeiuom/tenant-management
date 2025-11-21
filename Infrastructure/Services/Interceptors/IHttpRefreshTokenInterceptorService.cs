9469e868cf26f47e9d8c57a62dbd37dcb1eda049
Farhankaioum <keiuom.swe@gmail.com>
2025-11-21 16:50:45 +1100

    All services removed from Infrastructure

--- Infrastructure/Services/Interceptors/IHttpRefreshTokenInterceptorService.cs
+++
@@ -1,7 +0,0 @@
-namespace Infrastructure.Services.Interceptors;
-
-public interface IHttpRefreshTokenInterceptorService
-{
-    Task InterceptBeforeHttpRequestAsync(object sender, HttpClientInterceptorEventArgs eventArgs);
-    void RegisterEvent();
-}
