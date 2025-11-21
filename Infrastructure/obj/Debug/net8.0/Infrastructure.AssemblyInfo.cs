afe378b85f7a3e912d2029e0599db63088837363
Farhankaioum <keiuom.swe@gmail.com>
2025-11-21 17:43:15 +1100

    TenantDbContext added

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0313d80b20a9f36f64d54ab076fa5b94c9116d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98e12b430c11406d12044dc05604ce1001cb8547")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
