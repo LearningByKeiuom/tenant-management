4376d789715b4c22acbe9d4ab32673bcb54ce13b
Farhankaioum <keiuom.swe@gmail.com>
2025-11-21 16:15:18 +1100

    SchoolAction added

---
+++ Infrastructure/Constants/PermissionConstants.cs
@@ -0,0 +1,11 @@
+namespace Infrastructure.Constants;
+
+public static class SchoolAction
+{
+    public const string Read = nameof(Read);
+    public const string Create = nameof(Create);
+    public const string Update = nameof(Update);
+    public const string Delete = nameof(Delete);
+    public const string RefreshToken = nameof(RefreshToken);
+    public const string UpgradeSubscription = nameof(UpgradeSubscription);
+}
--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c66f9656bbf55c3802ba4083f475991d5cfb6b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11372bca8a8f0a268696f5f5cc70a7af12537d66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
