62f032093a608f7ec0dd1be13357458b0738d3c8
Farhankaioum <keiuom.swe@gmail.com>
2025-11-22 21:14:17 +1100

    RoleService added

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b674841bb7adc42eacdf7f4be3eaa6291b5977d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4a0adc812d10f6507802ead32d39134a742e689")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
