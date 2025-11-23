2125d1d2d44517525c517c56796b8f30f9a1d82e
Farhankaioum <keiuom.swe@gmail.com>
2025-11-23 23:58:47 +1100

    JwtSettings added

---
+++ Application/JwtSettings.cs
@@ -0,0 +1,7 @@
+namespace Application;
+
+public class JwtSettings
+{
+    public string Secret { get; set; }
+    public int TokenExpiryTimeInMinutes { get; set; }
+    public int RefreshTokenExpiryTimeInDays { get; set; }
--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+245f49178e112df388101fb67b0c4a8d403422ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e7a1f3945968b1aaea12fad1828c333651eef0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
