6759faa7c2555fcd8dfe13720d68058e6d2d8726
Farhankaioum <keiuom.swe@gmail.com>
2025-11-23 23:56:23 +1100

    Custom IdentityException added in Application

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a320b69fb347d1100dee558dc5fa72f1d082630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+521a50b7ccf37911657654058058db0c2fa032ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
