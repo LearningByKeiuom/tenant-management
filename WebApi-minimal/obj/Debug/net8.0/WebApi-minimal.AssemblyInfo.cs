b9bf3779ed2fdb44887ab5c2880f5fdcc4d457fb
Farhankaioum <keiuom.swe@gmail.com>
2025-11-23 23:50:53 +1100

    FluentValidation package added in Application

--- WebApi-minimal/obj/Debug/net8.0/WebApi-minimal.AssemblyInfo.cs
+++ WebApi-minimal/obj/Debug/net8.0/WebApi-minimal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi-minimal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9469e868cf26f47e9d8c57a62dbd37dcb1eda049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47041564238c8eced4434b5d99a435c05e70cdb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi-minimal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi-minimal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
