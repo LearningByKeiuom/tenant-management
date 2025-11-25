330b03d4452fe28e0b6016005711793a6a180e3d
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 02:52:50 +1100

    TenantResponse model added in Identity feature

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+324a6e19d4332ba65bcfaa017f4d8a4cfbce7dd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73fbc7265975da5844b680ec450621e362ac23d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
