1b1833b60576922f8a562d4061ee81d0904cfef8
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 01:00:06 +1100

    UserRoleResponse model added in Identity feature

---
+++ Application/Features/Identity/Users/UserRoleResponse.cs
@@ -0,0 +1,9 @@
+namespace Application.Features.Identity.Users;
+
+public class UserRoleResponse
+{
+    public string RoleId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool IsAssigned { get; set; }
+}
