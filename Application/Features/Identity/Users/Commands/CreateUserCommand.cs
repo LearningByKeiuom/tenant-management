b2ca9b4089d3038a64ba1bb99da9615e477c2f56
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 01:38:51 +1100

    CreateUserCommandHandler added in Identity feature

--- Application/Features/Identity/Users/Commands/CreateUserCommand.cs
+++ Application/Features/Identity/Users/Commands/CreateUserCommand.cs
@@ -7,3 +7,19 @@ public class CreateUserCommand : IRequest<IResponseWrapper>
 {
     public CreateUserRequest CreateUser { get; set; }
 }
+
+public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, IResponseWrapper>
+{
+    private readonly IUserService _userService;
+
+    public CreateUserCommandHandler(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    public async Task<IResponseWrapper> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var userId = await _userService.CreateAsync(request.CreateUser);
+        return await ResponseWrapper<string>.SuccessAsync(data: userId, message: "User created successfully.");
+    }
+}
