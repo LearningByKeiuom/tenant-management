241ad5fef6a6fac34e6014ae9a6163e244dc0b69
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:22:29 +1100

    DeleteRoleCommandHandler added om Identity feature

--- Application/Features/Identity/Roles/Commands/DeleteRoleCommand.cs
+++ Application/Features/Identity/Roles/Commands/DeleteRoleCommand.cs
@@ -7,3 +7,14 @@ public class DeleteRoleCommand : IRequest<IResponseWrapper>
 {
     public string RoleId { get; set; }
 }
+
+public class DeleteRoleCommandHandler(IRoleService roleService) : IRequestHandler<DeleteRoleCommand, IResponseWrapper>
+{
+    private readonly IRoleService _roleService = roleService;
+
+    public async Task<IResponseWrapper> Handle(DeleteRoleCommand request, CancellationToken cancellationToken)
+    {
+        var deletedRole = await _roleService.DeleteAsync(request.RoleId);
+        return await ResponseWrapper.SuccessAsync(message: $"Role '{deletedRole}' deleted successfully.");
+    }
+}
