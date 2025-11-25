e98584c74f5f0cc09fcd16c5c1ab94138b2182b7
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:32:22 +1100

    TokenRequest model added in Identity feature

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b2da0f7386889fed6e9f881da2274fe390a55a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce95d662fed909dcfe3d237668d6ceb37a226391")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
