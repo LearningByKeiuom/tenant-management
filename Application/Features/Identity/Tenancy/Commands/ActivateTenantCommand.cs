4bc1b8bb9987ef4b7cf47e79b053573cabc12222
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 02:58:50 +1100

    ActivateTenantCommandHandler added in Identity feature

--- Application/Features/Identity/Tenancy/Commands/ActivateTenantCommand.cs
+++ Application/Features/Identity/Tenancy/Commands/ActivateTenantCommand.cs
@@ -7,3 +7,19 @@ public class ActivateTenantCommand : IRequest<IResponseWrapper>
 {
     public string TenantId { get; set; }
 }
+
+public class ActivateTenantCommandHandler : IRequestHandler<ActivateTenantCommand, IResponseWrapper>
+{
+    private readonly ITenantService _tenantService;
+
+    public ActivateTenantCommandHandler(ITenantService tenantService)
+    {
+        _tenantService = tenantService;
+    }
+
+    public async Task<IResponseWrapper> Handle(ActivateTenantCommand request, CancellationToken cancellationToken)
+    {
+        var tenantId = await _tenantService.ActivateAsync(request.TenantId);
+        return await ResponseWrapper<string>.SuccessAsync(data: tenantId, "Tenant activation successful.");
+    }
+}
