4fd478b4f3c5217765ef8378b288b55d5689c3c5
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 01:31:59 +1100

    GetUserPermissionsQueryHandler added in Identity feature

--- Application/Features/Identity/Users/Queries/GetUserPermissionsQuery.cs
+++ Application/Features/Identity/Users/Queries/GetUserPermissionsQuery.cs
@@ -7,3 +7,15 @@ public class GetUserPermissionsQuery : IRequest<IResponseWrapper>
 {
     public string UserId { get; set; }
 }
+
+public class GetUserPermissionsQueryHandler(IUserService userService)
+    : IRequestHandler<GetUserPermissionsQuery, IResponseWrapper>
+{
+    private readonly IUserService _userService = userService;
+
+    public async Task<IResponseWrapper> Handle(GetUserPermissionsQuery request, CancellationToken cancellationToken)
+    {
+        var permissions = await _userService.GetUserPermissionsAsync(request.UserId, cancellationToken);
+        return await ResponseWrapper<List<string>>.SuccessAsync(data: permissions);
+    }
+}
