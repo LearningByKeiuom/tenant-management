b27af6ce0fd05808a45636de39c72bdeac50f9c4
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 02:08:42 +1100

    DeleteSchoolCommandHandler added in Identity feature

--- Application/Features/Identity/Schools/Commands/DeleteSchoolCommand.cs
+++ Application/Features/Identity/Schools/Commands/DeleteSchoolCommand.cs
@@ -7,3 +7,26 @@ public class DeleteSchoolCommand : IRequest<IResponseWrapper>
 {
     public int SchoolId { get; set; }
 }
+
+public class DeleteSchoolCommandHandler : IRequestHandler<DeleteSchoolCommand, IResponseWrapper>
+{
+    private readonly ISchoolService _schoolService;
+
+    public DeleteSchoolCommandHandler(ISchoolService schoolService)
+    {
+        _schoolService = schoolService;
+    }
+
+    public async Task<IResponseWrapper> Handle(DeleteSchoolCommand request, CancellationToken cancellationToken)
+    {
+        var schoolInDb = await _schoolService.GetByIdAsync(request.SchoolId);
+
+        if (schoolInDb is not null)
+        {
+            var deletedSchoolId = await _schoolService.DeleteAsync(schoolInDb);
+
+            return await ResponseWrapper<int>.SuccessAsync(data: deletedSchoolId, "School deleted successfully.");
+        }
+        return await ResponseWrapper<int>.FailAsync("School does not exist.");
+    }
+}
