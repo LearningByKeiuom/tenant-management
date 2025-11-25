bf1bd0daf315e40e707dafe6032ca1f9d17a3eb1
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 01:14:11 +1100

    Warning issue fixed for UpdateUserRequest

--- Application/Features/Identity/Users/UpdateUserRequest.cs
+++ Application/Features/Identity/Users/UpdateUserRequest.cs
@@ -2,8 +2,8 @@ namespace Application.Features.Identity.Users;
 
 public class UpdateUserRequest
 {
-    public string Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string PhoneNumber { get; set; }
+    public string Id { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
 }
