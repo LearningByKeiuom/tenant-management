709e35c995fc0ded770287fff0945c289e310278
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 02:38:01 +1100

    Warning issue fixed for SchoolResponse

--- Application/Features/Identity/Schools/SchoolResponse.cs
+++ Application/Features/Identity/Schools/SchoolResponse.cs
@@ -3,6 +3,6 @@ namespace Application.Features.Identity.Schools;
 public class SchoolResponse
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public DateTime EstablishedDate { get; set; }
 }
