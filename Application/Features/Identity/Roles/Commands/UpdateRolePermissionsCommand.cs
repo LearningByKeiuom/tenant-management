f7b2da0f7386889fed6e9f881da2274fe390a55a
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:30:38 +1100

    UpdateRolePermissionsCommandHandler added in Identity feature

--- Application/Features/Identity/Roles/Commands/UpdateRolePermissionsCommand.cs
+++ Application/Features/Identity/Roles/Commands/UpdateRolePermissionsCommand.cs
@@ -7,3 +7,15 @@ public class UpdateRolePermissionsCommand : IRequest<IResponseWrapper>
 {
     public UpdateRolePermissionsRequest UpdateRolePermissions { get; set; }
 }
+
+public class UpdateRolePermissionsCommandHandler(IRoleService roleService)
+    : IRequestHandler<UpdateRolePermissionsCommand, IResponseWrapper>
+{
+    private readonly IRoleService _roleService = roleService;
+
+    public async Task<IResponseWrapper> Handle(UpdateRolePermissionsCommand request, CancellationToken cancellationToken)
+    {
+        var message = await _roleService.UpdatePermissionsAsync(request.UpdateRolePermissions);
+        return await ResponseWrapper.SuccessAsync(message: message);
+    }
+}
