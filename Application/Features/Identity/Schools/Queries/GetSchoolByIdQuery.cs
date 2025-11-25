b02c36b8e4fe1dc76b577f9db99f59437a2246f4
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 02:40:11 +1100

    GetSchoolByIdQueryHandler added in Identity feature

--- Application/Features/Identity/Schools/Queries/GetSchoolByIdQuery.cs
+++ Application/Features/Identity/Schools/Queries/GetSchoolByIdQuery.cs
@@ -8,3 +8,18 @@ public class GetSchoolByIdQuery : IRequest<IResponseWrapper>
 {
     public int SchoolId { get; set; }
 }
+
+public class GetSchoolByIdQueryHandler : IRequestHandler<GetSchoolByIdQuery, IResponseWrapper>
+{
+    private readonly ISchoolService _schoolService;
+    public async Task<IResponseWrapper> Handle(GetSchoolByIdQuery request, CancellationToken cancellationToken)
+    {
+        var schoolInDb = await _schoolService.GetByIdAsync(request.SchoolId);
+
+        if (schoolInDb is not null)
+        {
+            return await ResponseWrapper<SchoolResponse>.SuccessAsync(data: schoolInDb.Adapt<SchoolResponse>());
+        }
+        return await ResponseWrapper<int>.FailAsync("School does not exist.");
+    }
+}
