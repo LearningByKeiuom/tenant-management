a12359b9af87e46abceef85bc3e7fe2b777b19cf
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 01:51:42 +1100

    UpdateSchoolRequest model added in Identity

---
+++ Application/Features/Identity/Schools/UpdateSchoolRequest.cs
@@ -0,0 +1,8 @@
+namespace Application.Features.Identity.Schools;
+
+public class UpdateSchoolRequest
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime EstablishedDate { get; set; }
+}
