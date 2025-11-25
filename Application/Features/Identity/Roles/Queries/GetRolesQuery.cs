a714dfec75d8634cdf334918ffa36877eefdf8ad
Farhankaioum <keiuom.swe@gmail.com>
2025-11-25 23:58:45 +1100

    GetRolesQueryHandler added in Identity feature

--- Application/Features/Identity/Roles/Queries/GetRolesQuery.cs
+++ Application/Features/Identity/Roles/Queries/GetRolesQuery.cs
@@ -6,3 +6,14 @@ namespace Application.Features.Identity.Roles.Queries;
 public class GetRolesQuery : IRequest<IResponseWrapper>
 {
 }
+
+public class GetRolesQueryHandler(IRoleService roleService) : IRequestHandler<GetRolesQuery, IResponseWrapper>
+{
+    private readonly IRoleService _roleService = roleService;
+
+    public async Task<IResponseWrapper> Handle(GetRolesQuery request, CancellationToken cancellationToken)
+    {
+        var roles = await _roleService.GetAllAsync(cancellationToken);
+        return await ResponseWrapper<List<RoleResponse>>.SuccessAsync(data: roles);
+    }
+}
