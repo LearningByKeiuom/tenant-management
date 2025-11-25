fc80cffed32814793a93c1a3593ea7b54af3d172
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:52:54 +1100

    ChangePasswordRequest model added in Identity

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e130dbc40cbb440ad02d8fa2a3601bba0827f12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fd493d8402b40fca838e5e0fedb5a17e525be1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
