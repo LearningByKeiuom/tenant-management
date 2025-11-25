5d2daeafb6ec169dd72a0f767e0c9570f79e2298
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:18:19 +1100

    Warning issue fixed for UpdateRolePermissionsRequest model in Identity feature

--- Application/Features/Identity/Roles/UpdateRolePermissionsRequest.cs
+++ Application/Features/Identity/Roles/UpdateRolePermissionsRequest.cs
@@ -2,6 +2,6 @@ namespace Application.Features.Identity.Roles;
 
 public class UpdateRolePermissionsRequest
 {
-    public string RoleId { get; set; }
+    public string RoleId { get; set; } = string.Empty;
     public List<string> NewPermissions { get; set; } = [];
 }
