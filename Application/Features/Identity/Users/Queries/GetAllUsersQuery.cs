c94b2a3b0fcb50bdd291ee40f599fe4b5dfa56f4
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 01:26:06 +1100

    GetAllUsersQueryHandler added in Identity feature

--- Application/Features/Identity/Users/Queries/GetAllUsersQuery.cs
+++ Application/Features/Identity/Users/Queries/GetAllUsersQuery.cs
@@ -6,3 +6,14 @@ namespace Application.Features.Identity.Users.Queries;
 public class GetAllUsersQuery : IRequest<IResponseWrapper>
 {
 }
+
+public class GetAllUsersQueryHandler(IUserService userService) : IRequestHandler<GetAllUsersQuery, IResponseWrapper>
+{
+    private readonly IUserService _userService = userService;
+
+    public async Task<IResponseWrapper> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+    {
+        var users = await _userService.GetAllAsync(cancellationToken);
+        return await ResponseWrapper<List<UserResponse>>.SuccessAsync(data: users);
+    }
+}
