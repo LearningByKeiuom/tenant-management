e1c6459a5652b394c47e011ed576e585f9bc981b
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 01:58:31 +1100

    CreateSchoolCommandHandler added in Identity feature

--- Application/Features/Identity/Schools/Commands/CreateSchoolCommand.cs
+++ Application/Features/Identity/Schools/Commands/CreateSchoolCommand.cs
@@ -10,3 +10,22 @@ public class CreateSchoolCommand : IRequest<IResponseWrapper>, IValidateMe
 {
     public CreateSchoolRequest CreateSchool { get; set; }
 }
+
+public class CreateSchoolCommandHandler : IRequestHandler<CreateSchoolCommand, IResponseWrapper>
+{
+    private readonly ISchoolService _schoolService;
+
+    public CreateSchoolCommandHandler(ISchoolService schoolService)
+    {
+        _schoolService = schoolService;
+    }
+
+    public async Task<IResponseWrapper> Handle(CreateSchoolCommand request, CancellationToken cancellationToken)
+    {
+        var newSchool = request.CreateSchool.Adapt<School>();
+
+        var schoolId = await _schoolService.CreateAsync(newSchool);
+
+        return await ResponseWrapper<int>.SuccessAsync(data: schoolId, "School created successfully.");
+    }
+}
