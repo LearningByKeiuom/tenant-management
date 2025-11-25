154c0727b31a05b00bb11e7f6108b5a9502ddfa1
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:29:19 +1100

    UpdateRoleCommandHandler added in Identity feature

--- Application/Features/Identity/Roles/Commands/UpdateRoleCommand.cs
+++ Application/Features/Identity/Roles/Commands/UpdateRoleCommand.cs
@@ -7,3 +7,14 @@ public class UpdateRoleCommand : IRequest<IResponseWrapper>
 {
     public UpdateRoleRequest UpdateRole { get; set; }
 }
+
+public class UpdateRoleCommandHandler(IRoleService roleService) : IRequestHandler<UpdateRoleCommand, IResponseWrapper>
+{
+    private readonly IRoleService _roleService = roleService;
+
+    public async Task<IResponseWrapper> Handle(UpdateRoleCommand request, CancellationToken cancellationToken)
+    {
+        var updatedRole = await _roleService.UpdateAsync(request.UpdateRole);
+        return await ResponseWrapper.SuccessAsync(message: $"Role '{updatedRole}' updated successfully.");
+    }
+}
