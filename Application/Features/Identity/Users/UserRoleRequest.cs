820cecf05208894a8c4646e57315e406e73c43d2
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:58:53 +1100

    UserRoleRequest model added in Identity

---
+++ Application/Features/Identity/Users/UserRoleRequest.cs
@@ -0,0 +1,9 @@
+namespace Application.Features.Identity.Users;
+
+public class UserRoleRequest
+{
+    public string RoleId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool IsAssigned { get; set; }
+}
