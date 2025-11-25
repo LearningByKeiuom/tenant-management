bbe2d1fb73684113ff20272b78e6ce47cd01da10
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 03:02:44 +1100

    UpdateTenantSubscriptionCommandHandler added in Identity feature

--- Application/Features/Identity/Tenancy/Commands/UpdateTenantSubscriptionCommand.cs
+++ Application/Features/Identity/Tenancy/Commands/UpdateTenantSubscriptionCommand.cs
@@ -7,3 +7,19 @@ public class UpdateTenantSubscriptionCommand : IRequest<IResponseWrapper>
 {
     public UpdateTenantSubscriptionRequest UpdateTenantSubscription { get; set; }
 }
+
+public class UpdateTenantSubscriptionCommandHandler : IRequestHandler<UpdateTenantSubscriptionCommand, IResponseWrapper>
+{
+    private readonly ITenantService _tenantService;
+
+    public UpdateTenantSubscriptionCommandHandler(ITenantService tenantService)
+    {
+        _tenantService = tenantService;
+    }
+
+    public async Task<IResponseWrapper> Handle(UpdateTenantSubscriptionCommand request, CancellationToken cancellationToken)
+    {
+        var tenantId = await _tenantService.UpdateSubscriptionAsync(request.UpdateTenantSubscription);
+        return await ResponseWrapper<string>.SuccessAsync(data: tenantId, "Tenant subscription updated successfully.");
+    }
+}
