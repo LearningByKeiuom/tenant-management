31ff7658cf4e2fd270eeb454de2fef50ef0eb103
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 01:47:26 +1100

    UpdateUserRolesCommandHandler added in Identity feature

--- Application/Features/Identity/Users/Commands/UpdateUserRolesCommand.cs
+++ Application/Features/Identity/Users/Commands/UpdateUserRolesCommand.cs
@@ -8,3 +8,14 @@ public class UpdateUserRolesCommand : IRequest<IResponseWrapper>
     public string UserId { get; set; }
     public UserRolesRequest UserRolesRequest { get; set; }
 }
+
+public class UpdateUserRolesCommandHandler(IUserService userService) : IRequestHandler<UpdateUserRolesCommand, IResponseWrapper>
+{
+    private readonly IUserService _userService = userService;
+
+    public async Task<IResponseWrapper> Handle(UpdateUserRolesCommand request, CancellationToken cancellationToken)
+    {
+        var userId = await _userService.AssignRolesAsync(request.UserId, request.UserRolesRequest);
+        return await ResponseWrapper<string>.SuccessAsync(data: userId, message: "User roles updated successfully.");
+    }
+}
