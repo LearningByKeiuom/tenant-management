d8fe86a65f4aca93a53c3443966569bbc7c9f13a
Farhankaioum <keiuom.swe@gmail.com>
2025-11-25 23:44:13 +1100

    Generic IResponseWrapper added

--- Application/Wrappers/IResponseWrapper.cs
+++ Application/Wrappers/IResponseWrapper.cs
@@ -5,3 +5,8 @@ public interface IResponseWrapper
     List<string> Messages { get; set; }
     bool IsSuccessful { get; set; }
 }
+
+public interface IResponseWrapper<out T> : IResponseWrapper
+{
+    T Data { get; }
+}
