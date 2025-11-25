2d6b6ee2aaa901532cf646826c4ca8a2db4b23f5
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:17:34 +1100

    Warning issue fixed for RoleResponse model

--- Application/Features/Identity/Roles/RoleResponse.cs
+++ Application/Features/Identity/Roles/RoleResponse.cs
@@ -2,8 +2,8 @@ namespace Application.Features.Identity.Roles;
 
 public class RoleResponse
 {
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; }  = string.Empty;
+    public string Description { get; set; }  = string.Empty;
     public List<string> Permissions { get; set; } = [];
 }
