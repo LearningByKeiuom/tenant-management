85d0430198f9aba8d1eb73406196d0109dd3480b
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 01:24:09 +1100

    Warning issue fixed for ChangePasswordRequest

--- Application/Features/Identity/Users/ChangePasswordRequest.cs
+++ Application/Features/Identity/Users/ChangePasswordRequest.cs
@@ -2,8 +2,8 @@ namespace Application.Features.Identity.Users;
 
 public class ChangePasswordRequest
 {
-    public string UserId { get; set; }
-    public string CurrentPassword { get; set; }
-    public string NewPassword { get; set; }
-    public string ConfirmNewPassword { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public string CurrentPassword { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+    public string ConfirmNewPassword { get; set; } = string.Empty;
 }
