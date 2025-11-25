00589d5584c600fdf73e1901f93ac20c9182f307
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 03:01:18 +1100

    DeactivateTenantCommandHandler added in Identity feature

--- Application/Features/Identity/Tenancy/Commands/DeactivateTenantCommand.cs
+++ Application/Features/Identity/Tenancy/Commands/DeactivateTenantCommand.cs
@@ -7,3 +7,20 @@ public class DeactivateTenantCommand : IRequest<IResponseWrapper>
 {
     public string TenantId { get; set; }
 }
+
+public class DeactivateTenantCommandHandler : IRequestHandler<DeactivateTenantCommand, IResponseWrapper>
+{
+    private readonly ITenantService _tenantService;
+
+    public DeactivateTenantCommandHandler(ITenantService tenantService)
+    {
+        _tenantService = tenantService;
+    }
+
+    public async Task<IResponseWrapper> Handle(DeactivateTenantCommand request, CancellationToken cancellationToken)
+    {
+        var tenantId = await _tenantService.DeactivateAsync(request.TenantId);
+
+        return await ResponseWrapper<string>.SuccessAsync(data: tenantId, "Tenant de-activation successful.");
+    }
+}
