d02bf1a5d36181a6f5c945b626ec36b9e1daffe0
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:33:41 +1100

    Warning issue fixed for RefreshTokenRequest model

--- Application/Features/Identity/Tokens/RefreshTokenRequest.cs
+++ Application/Features/Identity/Tokens/RefreshTokenRequest.cs
@@ -2,7 +2,7 @@ namespace Application.Features.Identity.Tokens;
 
 public class RefreshTokenRequest
 {
-    public string CurrentJwt { get; set; }
-    public string CurrentRefreshToken { get; set; }
+    public string CurrentJwt { get; set; } = string.Empty;
+    public string CurrentRefreshToken { get; set; }  = string.Empty;
     public DateTime RefreshTokenExpiryDate { get; set; }
 }
