b5df9ecd1bb9b93865b770ffdb68fbff673d843b
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 03:11:41 +1100

    Warning issue fixed for UpdateTenantSubscriptionRequest model

--- Application/Features/Tenancy/UpdateTenantSubscriptionRequest.cs
+++ Application/Features/Tenancy/UpdateTenantSubscriptionRequest.cs
@@ -2,6 +2,6 @@ namespace Application.Features.Tenancy;
 
 public class UpdateTenantSubscriptionRequest
 {
-    public string TenantId { get; set; }
+    public string TenantId { get; set; } = string.Empty;
     public DateTime NewExpiryDate { get; set; }
 }
