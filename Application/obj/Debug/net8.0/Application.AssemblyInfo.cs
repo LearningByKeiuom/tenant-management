7a119b549bd98a07a150e8576b97c072bc33fe28
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:37:00 +1100

    ITokenService added in Identity feature

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e98584c74f5f0cc09fcd16c5c1ab94138b2182b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88517820b7682db3a093ee24fac1944fe0b6a88d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
