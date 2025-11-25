88517820b7682db3a093ee24fac1944fe0b6a88d
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:34:58 +1100

    Warning issue fixed for TokenResponse model

--- Application/Features/Identity/Tokens/TokenResponse.cs
+++ Application/Features/Identity/Tokens/TokenResponse.cs
@@ -2,7 +2,7 @@ namespace Application.Features.Identity.Tokens;
 
 public class TokenResponse
 {
-    public string Jwt { get; set; }
-    public string RefreshToken { get; set; }
+    public string Jwt { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
     public DateTime RefreshTokenExpiryDate { get; set; }
 }
