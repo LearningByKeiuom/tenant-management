b28e1273324b203d77ee98a6c3bf522156f25b7b
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:19:28 +1100

    Warning issue fixed for UpdateRoleRequest model in Identity feature

--- Application/Features/Identity/Roles/UpdateRoleRequest.cs
+++ Application/Features/Identity/Roles/UpdateRoleRequest.cs
@@ -2,7 +2,7 @@ namespace Application.Features.Identity.Roles;
 
 public class UpdateRoleRequest
 {
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; }  = string.Empty;
+    public string Description { get; set; }   = string.Empty;
 }
