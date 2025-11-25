7bbef6f30e04a97821cfa99c61d503e6b8ca2286
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 02:49:59 +1100

    UpdateSchoolRequestValidator added in Identity feature

---
+++ Application/Features/Identity/Schools/Validations/UpdateSchoolRequestValidator.cs
@@ -0,0 +1,24 @@
+using Domain.Entities;
+using FluentValidation;
+
+namespace Application.Features.Identity.Schools.Validations;
+
+internal class UpdateSchoolRequestValidator : AbstractValidator<UpdateSchoolRequest>
+{
+    public UpdateSchoolRequestValidator(ISchoolService schoolService)
+    {
+        RuleFor(request => request.Id)
+            .NotEmpty()
+            .MustAsync(async (id, ct) => await schoolService.GetByIdAsync(id) is School schoolInDb && schoolInDb.Id == id)
+            .WithMessage("School does not exist.");
+
+        RuleFor(request => request.Name)
+            .NotEmpty()
+            .WithMessage("School name is required.")
+            .MaximumLength(60);
+
+        RuleFor(request => request.EstablishedDate)
+            .LessThanOrEqualTo(DateTime.UtcNow)
+            .WithMessage("Date established cannot be future date.");
+    }
+}
