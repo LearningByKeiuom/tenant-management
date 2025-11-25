b10c343843dc0473d938d97ba67198a9e3395043
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 01:42:33 +1100

    UpdateUserCommandHandler added in Identity feature

--- Application/Features/Identity/Users/Commands/UpdateUserCommand.cs
+++ Application/Features/Identity/Users/Commands/UpdateUserCommand.cs
@@ -7,3 +7,14 @@ public class UpdateUserCommand : IRequest<IResponseWrapper>
 {
     public UpdateUserRequest UpdateUser { get; set; }
 }
+
+public class UpdateUserCommandHandler(IUserService userService) : IRequestHandler<UpdateUserCommand, IResponseWrapper>
+{
+    private readonly IUserService _userService = userService;
+
+    public async Task<IResponseWrapper> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        var userId = await _userService.UpdateAsync(request.UpdateUser);
+        return await ResponseWrapper<string>.SuccessAsync(data: userId, message: "User updated successfully.");
+    }
+}
