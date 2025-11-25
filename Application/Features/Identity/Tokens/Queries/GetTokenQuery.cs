2c1778fdab4a63e14251903d6584fe02c293902a
Farhankaioum <keiuom.swe@gmail.com>
2025-11-26 00:40:05 +1100

    GetTokenQuery added in Identity feature

---
+++ Application/Features/Identity/Tokens/Queries/GetTokenQuery.cs
@@ -0,0 +1,9 @@
+using Application.Wrappers;
+using MediatR;
+
+namespace Application.Features.Identity.Tokens.Queries;
+
+public class GetTokenQuery : IRequest<IResponseWrapper>
+{
+    public TokenRequest TokenRequest { get; set; }
+}
