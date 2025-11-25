4426716ba4fa85d50d4adbc4d76aa9f05a418534
Farhankaioum <keiuom.swe@gmail.com>
2025-11-25 23:45:49 +1100

    Generic ResponseWrapper implementation added

--- Application/Wrappers/ResponseWrapper.cs
+++ Application/Wrappers/ResponseWrapper.cs
@@ -78,3 +78,76 @@ public class ResponseWrapper : IResponseWrapper
             return Task.FromResult(Success(messages));
         }
     }
+    
+    public class ResponseWrapper<T> : ResponseWrapper, IResponseWrapper<T>
+    {
+        public ResponseWrapper() { }
+        public T Data { get; set; }
+
+        public new static ResponseWrapper<T> Fail()
+        {
+            return new ResponseWrapper<T> { IsSuccessful = false };
+        }
+        public new static ResponseWrapper<T> Fail(string message)
+        {
+            return new ResponseWrapper<T> { IsSuccessful = false, Messages = [message] };
+        }
+        public new static ResponseWrapper<T> Fail(List<string> messages)
+        {
+            return new ResponseWrapper<T> { IsSuccessful = false, Messages = messages };
+        }
+        public new static Task<ResponseWrapper<T>> FailAsync()
+        {
+            return Task.FromResult(Fail());
+        }
+        public new static Task<ResponseWrapper<T>> FailAsync(string message)
+        {
+            return Task.FromResult(Fail(message));
+        }
+        public new static Task<ResponseWrapper<T>> FailAsync(List<string> messages)
+        {
+            return Task.FromResult(Fail(messages));
+        }
+
+        public new static ResponseWrapper<T> Success()
+        {
+            return new ResponseWrapper<T> { IsSuccessful = true };
+        }
+        public new static ResponseWrapper<T> Success(string message)
+        {
+            return new ResponseWrapper<T> { IsSuccessful = true, Messages = [message] };
+        }
+        public static ResponseWrapper<T> Success(T data)
+        {
+            return new ResponseWrapper<T> { IsSuccessful = true, Data = data };
+        }
+        public static ResponseWrapper<T> Success(T data, string message)
+        {
+            return new ResponseWrapper<T> { IsSuccessful = true, Data = data, Messages = [ message ] };
+        }
+        public static ResponseWrapper<T> Success(T data, List<string> messages)
+        {
+            return new ResponseWrapper<T> { IsSuccessful = true, Data = data, Messages = messages };
+        }
+
+        public new static Task<ResponseWrapper<T>> SuccessAsync()
+        {
+            return Task.FromResult(Success());
+        }
+        public new static Task<ResponseWrapper<T>> SuccessAsync(string message)
+        {
+            return Task.FromResult(Success(message));
+        }
+        public static Task<ResponseWrapper<T>> SuccessAsync(T data)
+        {
+            return Task.FromResult(Success(data));
+        }
+        public static Task<ResponseWrapper<T>> SuccessAsync(T data, string message)
+        {
+            return Task.FromResult(Success(data, message));
+        }
+        public static Task<ResponseWrapper<T>> SuccessAsync(T data, List<string> messages)
+        {
+            return Task.FromResult(Success(data, messages));
+        }
+    }
