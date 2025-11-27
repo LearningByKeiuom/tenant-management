ff693d6dc2220acd0ea53915cc0e8db70542964c
Farhankaioum <keiuom.swe@gmail.com>
2025-11-27 23:33:34 +1100

    IRoleService inherit in RoleService

--- Infrastructure/Identity/RoleService.cs
+++ Infrastructure/Identity/RoleService.cs
@@ -1,6 +1,46 @@
+using Application.Features.Identity.Roles;
+
 namespace Infrastructure.Identity;
 
-public class RoleService
+public class RoleService : IRoleService
 {
-    
+    public Task<string> CreateAsync(CreateRoleRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> UpdateAsync(UpdateRoleRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> DeleteAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> UpdatePermissionsAsync(UpdateRolePermissionsRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DoesItExistsAsync(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<RoleResponse>> GetAllAsync(CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RoleResponse> GetByIdAsync(string id, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RoleResponse> GetRoleWithPermissionsAsync(string id, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
 }
