82bbeb3259be14e6b750f0516d495f9e81a618e1
Farhankaioum <keiuom.swe@gmail.com>
2025-11-27 23:32:34 +1100

    ITokenService inherit in TokenService

--- Infrastructure/Identity/Tokens/TokenService.cs
+++ Infrastructure/Identity/Tokens/TokenService.cs
@@ -1,8 +1,16 @@
-
+using Application.Features.Identity.Tokens;
 
 namespace Infrastructure.Identity.Tokens;
 
-public class TokenService
+public class TokenService : ITokenService
 {
-    
+    public Task<TokenResponse> LoginAsync(TokenRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TokenResponse> RefreshTokenAsync(RefreshTokenRequest request)
+    {
+        throw new NotImplementedException();
+    }
 }
