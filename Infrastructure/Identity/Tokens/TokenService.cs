10a3f8a92b6d612d80480e6fed676eab2f27f385
Farhankaioum <keiuom.swe@gmail.com>
2025-11-28 23:50:29 +1100

    Services inject in TokenService

--- Infrastructure/Identity/Tokens/TokenService.cs
+++ Infrastructure/Identity/Tokens/TokenService.cs
@@ -1,9 +1,40 @@
 using Application.Features.Identity.Tokens;
+using Application;
+using Application.Exceptions;
+using Application.Features.Identity.Tokens;
+using Finbuckle.MultiTenant.Abstractions;
+using Infrastructure.Constants;
+using Infrastructure.Identity.Models;
+using Infrastructure.Tenancy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Infrastructure.Identity.Tokens;
 
 public class TokenService : ITokenService
 {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<ApplicationRole> _roleManager;
+    private readonly IMultiTenantContextAccessor<ABCSchoolTenantInfo> _tenantContextAccessor;
+    private readonly JwtSettings _jwtSettings;
+
+    public TokenService(
+        UserManager<ApplicationUser> userManager,
+        IMultiTenantContextAccessor<ABCSchoolTenantInfo> tenantContextAccessor,
+        RoleManager<ApplicationRole> roleManager,
+        IOptions<JwtSettings> jwtSettings)
+    {
+        _userManager = userManager;
+        _tenantContextAccessor = tenantContextAccessor;
+        _roleManager = roleManager;
+        _jwtSettings = jwtSettings.Value;
+    }
+    
     public Task<TokenResponse> LoginAsync(TokenRequest request)
     {
         throw new NotImplementedException();
