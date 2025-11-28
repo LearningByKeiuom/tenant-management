92971defe703e0e3f6a508fd815f519ddab696e8
Farhankaioum <keiuom.swe@gmail.com>
2025-11-29 00:58:41 +1100

    Permissions property make nullable in RoleResponse model

--- Application/Features/Identity/Roles/RoleResponse.cs
+++ Application/Features/Identity/Roles/RoleResponse.cs
@@ -5,5 +5,5 @@ public class RoleResponse
     public string Id { get; set; } = string.Empty;
     public string Name { get; set; }  = string.Empty;
     public string Description { get; set; }  = string.Empty;
-    public List<string> Permissions { get; set; } = [];
+    public List<string?> Permissions { get; set; } = [];
 }
