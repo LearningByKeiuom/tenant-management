96b7baa349f3104f11cae43b44021dfb61076dfa
Farhankaioum <keiuom.swe@gmail.com>
2025-12-01 00:12:51 +1100

    GetTokenAsync added in TokenController

--- WebAPI-v2/Controllers/TokenController.cs
+++ WebAPI-v2/Controllers/TokenController.cs
@@ -1,10 +1,30 @@
-using Microsoft.AspNetCore.Http;
+using Application.Features.Identity.Tokens;
+using Application.Features.Identity.Tokens.Queries;
+using Infrastructure.Constants;
+using Infrastructure.Identity.Auth;
+using Infrastructure.OpenApi;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
 
 namespace WebAPI_v2.Controllers
 {
     [Route("api/[controller]")]
     public class TokenController : BaseApiController
     {
+        [HttpPost("login")]
+        [AllowAnonymous]
+        [TenantHeader]
+        [OpenApiOperation("Used to obtain jwt for login.")]
+        public async Task<IActionResult> GetTokenAsync([FromBody] TokenRequest tokenRequest)
+        {
+            var response = await Sender.Send(new GetTokenQuery { TokenRequest = tokenRequest });
+
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            } 
+            return BadRequest(response);
+        }
     }
 }
