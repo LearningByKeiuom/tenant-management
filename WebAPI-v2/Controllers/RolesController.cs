8dc3ce56771ee1616b4ec62c1fe14ab1fbd6bc4a
Farhankaioum <keiuom.swe@gmail.com>
2025-11-30 23:56:38 +1100

    AddRoleAsync controller method added in RolesController

--- WebAPI-v2/Controllers/RolesController.cs
+++ WebAPI-v2/Controllers/RolesController.cs
@@ -1,10 +1,25 @@
-using Microsoft.AspNetCore.Http;
+using Application.Features.Identity.Roles.Commands;
+using Application.Features.Identity.Roles;
+using Infrastructure.Constants;
+using Infrastructure.Identity.Auth;
 using Microsoft.AspNetCore.Mvc;
+using Application.Features.Identity.Roles.Queries;
 
 namespace WebAPI_v2.Controllers
 {
     [Route("api/[controller]")]
     public class RolesController : BaseApiController
     {
+        [HttpPost("add")]
+        [ShouldHavePermission(SchoolAction.Create, SchoolFeature.Roles)]
+        public async Task<IActionResult> AddRoleAsync([FromBody] CreateRoleRequest createRole)
+        {
+            var response = await Sender.Send(new CreateRoleCommand { CreateRole = createRole });
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+        }
     }
 }
