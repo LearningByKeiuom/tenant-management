bdd4548e6a6d5c5cd0eb8504b7c4061467e98ff8
Farhankaioum <keiuom.swe@gmail.com>
2025-12-01 00:10:17 +1100

    UpgradeTenantSubscriptionAsync added in TenantsController

--- WebAPI-v2/Controllers/TenantsController.cs
+++ WebAPI-v2/Controllers/TenantsController.cs
@@ -45,5 +45,17 @@ namespace WebAPI_v2.Controllers
             }
             return BadRequest(response);
         }
+        
+        [HttpPut("upgrade")]
+        [ShouldHavePermission(SchoolAction.UpgradeSubscription, SchoolFeature.Tenants)]
+        public async Task<IActionResult> UpgradeTenantSubscriptionAsync([FromBody] UpdateTenantSubscriptionRequest updateTenant)
+        {
+            var response = await Sender.Send(new UpdateTenantSubscriptionCommand { UpdateTenantSubscription = updateTenant });
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+        }
     }
 }
