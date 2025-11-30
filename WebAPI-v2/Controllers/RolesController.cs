e3159e130d1b0584abad98aebcac9c79df1b4eac
Farhankaioum <keiuom.swe@gmail.com>
2025-11-30 23:55:47 +1100

    RolesController added

---
+++ WebAPI-v2/Controllers/RolesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_v2.Controllers
+{
+    [Route("api/[controller]")]
+    public class RolesController : BaseApiController
+    {
+    }
+}
