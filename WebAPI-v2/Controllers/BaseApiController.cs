938714524abcb6003e20d12a983dd3c860f8ddb2
Farhankaioum <keiuom.swe@gmail.com>
2025-11-30 23:54:46 +1100

    BaseApiController added

---
+++ WebAPI-v2/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_v2.Controllers
+{
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        private ISender _sender = null!;
+
+        public ISender Sender => _sender ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
